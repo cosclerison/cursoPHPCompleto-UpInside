a65e63c605b3fe3fa459873c5033b51f4a0c5734
Clerison <cosclerison@gmail.com>
2023-07-08 23:03:48 -0300

    Trabalhando com Funções

--- 02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/index.php
+++ 02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/index.php
@@ -7,20 +7,37 @@ fullStackPHPClassName("02.11 - Trabalhando com funções");
  */
 fullStackPHPClassSession("functions", __LINE__);
 
+require __DIR__ . "/functions.php";
+
+var_dump(functionName("Clerison", "Juliana", "Bianca"), "<br><br>");
+var_dump(functionName("Tiger", "Maggie", "Peixe"), "<br><br>");
+
+var_dump(optionArgs("Clerison"), "<br><br>");
+var_dump(optionArgs("Clerison", "Tiger"), "<br><br>");
+var_dump(optionArgs("Clerison", "Maggie", "Peixe"), "<br><br>");
 
 /*
  * [ global access ] global $var
  */
 fullStackPHPClassSession("global access", __LINE__);
 
+$weight = 65;
+$height = 1.65;
+echo calcImc();
 
 /*
  * [ static arguments ] static $var
  */
 fullStackPHPClassSession("static arguments", __LINE__);
 
+$pay = payTotal(200);
+$pay = payTotal(150);
+
+echo $pay;
 
 /*
  * [ dinamic arguments ] get_args | num_args
  */
 fullStackPHPClassSession("dinamic arguments", __LINE__);
+
+var_dump(myTeam("Clerison", "Juliana", "Bianca", "Tiger", "Maggie", "Peixes"));
